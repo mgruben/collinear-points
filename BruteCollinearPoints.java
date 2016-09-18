8120481306a1ff22e597a154b7016519a46e87c3
mgruben <GrubenM@gmail.com>
2016-09-18 17:27:45 -0500

    Store collinear segments in an array

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -20,19 +20,27 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class BruteCollinearPoints {
-    private int segments;
+    private int segmentCount;
+    private LineSegment[] segments;
+    private int segmentHead;
 
     /**
      * Finds all line segments containing 4 points
      * @param points 
      */
     public BruteCollinearPoints(Point[] points) {
-        segments = 0;
+        segmentCount = 0;
+        segments = new LineSegment[points.length / 4];
+        segmentHead = 0;
         for (int i = 0; i < points.length - 3; i++) {
             double slopeA = points[i].slopeTo(points[i + 1]);
             double slopeB = points[i].slopeTo(points[i + 2]);
             double slopeC = points[i].slopeTo(points[i + 3]);
-            if (slopeA == slopeB && slopeB == slopeC) segments++;
+            if (slopeA == slopeB && slopeB == slopeC) {
+                segmentCount++;
+                segments[segmentHead++] = new LineSegment(points[i], points[i + 3]);
+                i += 3;
+            }
         }
     }
     
@@ -41,7 +49,7 @@ public class BruteCollinearPoints {
      * @return 
      */
     public int numberOfSegments() {
-        
+        return segmentCount;
     }
     
     /**
@@ -49,6 +57,16 @@ public class BruteCollinearPoints {
      * @return 
      */
     public LineSegment[] segments() {
-        
+        return segments;
+    }
+    
+    public static void main(String[] args) {
+        Point[] points = new Point[4];
+        points[0] = new Point(0, 0);
+        points[1] = new Point(0, 2);
+        points[2] = new Point(0, 1);
+        points[3] = new Point(0, 3);
+        BruteCollinearPoints b = new BruteCollinearPoints(points);
+        System.out.println(b.segments[0]);
     }
 }
