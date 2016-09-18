e55648a85b92c887efd579e15009db49cad221df
mgruben <GrubenM@gmail.com>
2016-09-18 14:54:42 -0500

    Initialize BruteCollinearPoints class

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class BruteCollinearPoints {
+   
+    /**
+     * Finds all line segments containing 4 points
+     * @param points 
+     */
+    public BruteCollinearPoints(Point[] points) {
+        
+    }
+    
+    /**
+     * The number of line segments
+     * @return 
+     */
+    public int numberOfSegments() {
+        
+    }
+    
+    /**
+     * The line segments
+     * @return 
+     */
+    public LineSegment[] segments() {
+        
+    }
+}
