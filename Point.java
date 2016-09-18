76ab4dc041d6fb43515669c54c443fffc575ecfa
mgruben <GrubenM@gmail.com>
2016-09-18 14:43:25 -0500

    Replace Point with Coursera-provided starter class

--- Point.java
+++ Point.java
@@ -1,85 +1,131 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *  
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
 
 import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
 
-/*
- * Copyright (C) 2016 Michael <GrubenM@GMail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- *
- * @author Michael <GrubenM@GMail.com>
- */
 public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
     /**
-     * "Constructs the point (x, y)"
-     * @param x
-     * @param y 
+     * Initializes a new point.
+     *
+     * @param  x the <em>x</em>-coordinate of the point
+     * @param  y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
-        
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
     }
-    
+
     /**
-     * "Draws this point"
+     * Draws this point to standard draw.
      */
     public void draw() {
-        
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
     }
-    
+
     /**
-     * "Draws the line segment from this point to that point"
-     * @param that 
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
      */
     public void drawTo(Point that) {
-        
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
-    
-    @Override
+
     /**
-     * "String representation"
-     * @return 
-     */
-    public String toString() {
-        
-    }
-    
-    @Override
-    /**
-     * "Compare two points by y-coordinates, breaking ties by x-coordinates"
-     * @param that
-     * @return 
-     */
-    public int compareTo(Point that) {
-        
-    }
-    
-    /**
-     * "The slope between this point and that point"
-     * @param that
-     * @return 
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        
+        /* YOUR CODE HERE */
     }
-    
+
     /**
-     * "Compare two points by slopes they make with this point"
-     * @return 
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        
+        /* YOUR CODE HERE */
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * You should not call the hashCode() method on this assignment.
+     * This means that you should not use java.util.HashMap or java.util.HashSet.
+     *
+     * @return a string representation of this point
+     */
+    public int hashCode() {
+        /* DO NOT MODIFY */
+        throw new UnsupportedOperationException("calling hashCode() is not permitted on this assignment");
+    }
+
+    /**
+     * You should not call the equals() method on this assignment.
+     * This means that you should not use java.util.TreeMap or java.util.TreeSet.
+     *
+    public boolean equals(Object that) {
+        /* DO NOT MODIFY */
+        throw new UnsupportedOperationException("calling equals() is not permitted on this assignment");
+    }
+
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
     }
 }
