9e231d7ca1b7b9c957b1b8f803bb1e635877e1c0
mgruben <GrubenM@gmail.com>
2016-09-19 07:42:02 -0500

    Remove unused import

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -3,7 +3,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
-import java.util.Comparator;
 
 /*
  * Copyright (C) 2016 Michael <GrubenM@GMail.com>
