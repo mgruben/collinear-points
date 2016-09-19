6ad6122e144a968196daed41323982f7e490d76a
mgruben <GrubenM@gmail.com>
2016-09-19 07:40:35 -0500

    Add sample client for testing

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,4 +1,7 @@
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 
 /*
@@ -84,12 +87,32 @@ public class BruteCollinearPoints {
     }
     
     public static void main(String[] args) {
-        Point[] points = new Point[4];
-        points[0] = new Point(0, 2);
-        points[1] = new Point(0, 0);
-        points[2] = new Point(0, 3);
-        points[3] = new Point(0, 1);
-        BruteCollinearPoints b = new BruteCollinearPoints(points);
-        System.out.println(b.segments[0]);
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,4 +1,7 @@
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -86,6 +89,8 @@ public class FastCollinearPoints {
      * "Throw a java.lang.NullPointerException if the client attempts to add a
      * null item"
      * 
+     * Quietly refuses to add duplicate LineSegments.
+     * 
      * Also doubles the length of the array when it is full.
      */
     private void enqueue(LineSegment l)
@@ -170,18 +175,32 @@ public class FastCollinearPoints {
     }
     
     public static void main(String[] args) {
-        Point[] points = new Point[9];
-        points[0] = new Point(1, 0);
-        points[1] = new Point(1, 1);
-        points[2] = new Point(1, 2);
-        points[3] = new Point(1, 3);
-        points[4] = new Point(1, 4);
-        points[5] = new Point(2, 1);
-        points[6] = new Point(3, 1);
-        points[7] = new Point(0, 1);
-        points[8] = new Point(-1, 1);
-        FastCollinearPoints f = new FastCollinearPoints(points);
-        for (LineSegment l: f.segments())
-            System.out.println(l);
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
