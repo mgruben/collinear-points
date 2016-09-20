fc129c121a7ed1d0c3b0afc044aa417ef50c8ecf
mgruben <GrubenM@gmail.com>
2016-09-19 20:40:44 -0500

    Add document comments

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -30,7 +30,7 @@ public class FastCollinearPoints {
     private int segmentSize;
     private Point[] collinear;
     private int collinearSize;
-    private Point[] pts;
+    private Point[] pts; // mutates for each new "origin"
     
     /**
      * Finds all line segments containing 4 or more points
@@ -38,28 +38,59 @@ public class FastCollinearPoints {
      */
     public FastCollinearPoints(Point[] points) {
         if (points == null) throw new java.lang.NullPointerException();
-        Point[] ptsUnsorted = points.clone();
         
-        Arrays.sort(ptsUnsorted);
-        for (int i = 0; i < ptsUnsorted.length - 1; i++) {
-            if (ptsUnsorted[i] == null) throw new java.lang.NullPointerException();
-            if (ptsUnsorted[i].compareTo(ptsUnsorted[i + 1]) == 0)
+        /**
+         * We have three points arrays: points, ptsUnsorted, and pts.
+         * 
+         * Checking for null points goes way faster if we've already sorted
+         * the points array, but the API requires that we can't mutate the
+         * incoming points.  Hence we make ptsNullCheck, and sort that.
+         * 
+         * Additionally, when we're iterating over each point in our main
+         * for loop below, we need to have consistent indexing, or the
+         * loop will miss some points and/or consider some points more than
+         * once.  Hence we create pts, and re-sort that for each new point
+         * under consideration, according to the next point in ptsNullCheck.
+         */
+        Point[] ptsNullCheck = points.clone();
+        Arrays.sort(ptsNullCheck);
+        for (int i = 0; i < ptsNullCheck.length - 1; i++) {
+            if (ptsNullCheck[i] == null) throw new java.lang.NullPointerException();
+            if (ptsNullCheck[i].compareTo(ptsNullCheck[i + 1]) == 0)
                 throw new java.lang.IllegalArgumentException();
         }
         
+        // Store the segments we've found in a ResizingArray
         segments = new LineSegment[1];
         segmentSize = 0;
-        pts = ptsUnsorted.clone();
         
-        for (int i = 0; i < pts.length; i++) {
-            Arrays.sort(pts, ptsUnsorted[i].slopeOrder());
+        pts = ptsNullCheck.clone();
+        
+        for (int p = 0; p < pts.length; p++) {
+            Arrays.sort(pts, ptsNullCheck[p].slopeOrder());
+            /**
+             * pts[0] is the current point, the slope to which was used to sort
+             * all other points in the array.  Thus, we start j at 1 so we don't
+             * compare the current point to itself, which wouldn't yield
+             * meaningful information.
+             */
             for (int j = 1; j < pts.length; j++) {
+                /**
+                 * Store collinear points in a ResizingArray.
+                 * It will probably need to hold 4 Points.
+                 */
                 collinear = new Point[4];
                 collinearSize = 0;
+                
+                /**
+                 * Make our first comparison, and add these points to collinear
+                 * (any two points are collinear)
+                 */
                 double slopeA = pts[0].slopeTo(pts[j]);
                 enqueue(pts[0]);
                 enqueue(pts[j]);
                 int c = 1;
+                
                 while (++j < pts.length && 
                         slopeA == pts[0].slopeTo(pts[j])) {
                     c++;
